6d7b1ec96a3089dc5c285bb2d79f9b883394c5e6
dkornai <danielkornai@gmail.com>
2023-06-23 19:01:44 +0100

    bugfixes and cleanups in the c++ module

--- simulate_gillespie_cpp.cpp
+++ simulate_gillespie_cpp.cpp
@@ -8,22 +8,30 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/eigen.h>
+
 #include <iostream>
+#include <string> 
 
 namespace py = pybind11;
 
 
-// set up shorthands for types
-using int64 = long;
-using float64 = double;
+// CUSTOM TYPES //
 
-using int64_1d = std::vector<int64>;
-using float64_1d = std::vector<float64>;
+using vec_long = std::vector<long>;
+using vec_double = std::vector<double>;
 
-typedef Eigen::Matrix<long, -1, -1> MatrixXl;
+typedef py::array_t<double> np_vec_f64; // 1d np.float64 array
+typedef py::array_t<long> np_vec_i64; // 1d np.int64 array 
 
-float64_1d get_flt_vec_from_np(
-    const   py::array_t<double> arr
+typedef Eigen::Matrix<long, -1, -1> MatrixXl; // matrix with long entries
+typedef Eigen::Ref<MatrixXl> np_mat_long; // 2d np.int64 array 
+
+
+// HELPER FUNCTIONS //
+
+// c++ vector from np.float64 array
+vec_double get_double_vec_from_np(
+    const   np_vec_f64   arr
     )
 {
     py::buffer_info info = arr.request();
@@ -33,8 +41,9 @@ float64_1d get_flt_vec_from_np(
     return vec;
 }
 
-int64_1d get_int_vec_from_np(
-    const   py::array_t<int64>  arr
+// c++ vector from np.int64 array
+vec_long get_long_vec_from_np(
+    const   np_vec_i64     arr
     )
 {
     py::buffer_info info = arr.request();
@@ -46,8 +55,8 @@ int64_1d get_int_vec_from_np(
 
 
 // draw samples from an exponential with a specified mean 
-float64 random_sample_exponential(
-    const   float64     mean
+double random_sample_exponential(
+    const   double     mean
     ) 
 {
     // initialized only once the first time the function is called
@@ -61,7 +70,7 @@ float64 random_sample_exponential(
 
 // function to sample an index from an array of probabilities associated with reactions
 int random_event_pmf(
-    const   float64_1d& react_probal
+    const   vec_double& react_probal
     ) 
 {
     // initialized only once the first time the function is called
@@ -74,87 +83,92 @@ int random_event_pmf(
 }
 
 
-// MAIN GILLESPIE FUNCTION
+// MAIN GILLESPIE FUNCTION //
 void simulate(
-    const   py::array_t<double> v_time_points,
-            py::array_t<double> v_react_rates,
-    const   py::array_t<long>   v_state_index,
-            py::array_t<long>   v_sys_state, 
-    const   int64       n_pops,
-    const   int64       n_reactions,
-            Eigen::Ref<MatrixXl> sys_state_sample,
-    const   Eigen::Ref<MatrixXl> reactions,
-    const   py::array_t<double> v_birthrate_updates_par
+    const   np_vec_f64      in_time_points,             // points in time where system state should be recorded
+    
+    const   np_vec_i64      in_sys_state,               // starting 'sys_state'
+            np_mat_long     sys_state_sample,           // array where the 'sys_state' is recorded at each time in 'time_points'
+            
+    const   np_mat_long     reactions,                  // 'sys_state' updates corresponding to each possible reaction
+    const   np_vec_f64      in_react_rates,             // per capita reaction rates
+    const   np_vec_i64      in_state_index,             // indeces of system state variables from which propensity is calculated
+    const   np_vec_f64      in_birthrate_updates_par    // parameters used to update dynamic birth rates
     )
 {
+    // c++ versions of numpy arrays
+    const   vec_double      time_points     = get_double_vec_from_np(in_time_points);
+            vec_double      react_rates     = get_double_vec_from_np(in_react_rates);
+    const   vec_long        state_index     = get_long_vec_from_np(in_state_index);
+            vec_long        sys_state       = get_long_vec_from_np(in_sys_state);
+    const   vec_double      br_up_par       = get_double_vec_from_np(in_birthrate_updates_par);
+
+    // counts often accessed during loops
+    const   int             n_time_points   = time_points.size();
+    const   int             n_pops          = sys_state.size();
+    const   int             n_reactions     = state_index.size();
+
+    // variables used in calculating the dynamic birth rate in compartment 0
+    const   double          c_b             = br_up_par[0];
+    const   double          mu              = br_up_par[1];
+    const   double          nss             = br_up_par[2];
+    const   double          delta           = br_up_par[3];
     
-    const   float64_1d  time_points = get_flt_vec_from_np(v_time_points);
-            float64_1d  react_rates = get_flt_vec_from_np(v_react_rates);
-    const   int64_1d    state_index = get_int_vec_from_np(v_state_index);
-            int64_1d    sys_state   = get_int_vec_from_np(v_sys_state);
-    const   float64_1d  birthrate_updates_par = get_flt_vec_from_np(v_birthrate_updates_par);
+    // variables rewritten throughout simulation
+            double          birth_rate;
+            long            reaction_index;
 
-    const int n_time_points = time_points.size();
+            vec_double      react_propen(n_reactions);
+            vec_double      react_probal(n_reactions);
 
-    const float64     c_b = birthrate_updates_par[0];
-    const float64     mu = birthrate_updates_par[1];
-    const float64     nss = birthrate_updates_par[2];
-    const float64     delta = birthrate_updates_par[3];
-    
-    float64     birth_rate;
-    int64       reaction_index;
-
-    float64_1d  react_propen(n_reactions);
-    float64_1d  react_probal(n_reactions);
-
-    float64     propensity_sum = 0.0;
-    float64     t = time_points[0];
-
-    // test section
-    if (sys_state[0] != 9) {
-        throw std::runtime_error("Test failed!");
-    }
+            double          propensity_sum  = 0.0;
+            double          t               = time_points[0];
 
 
+    // actual simulation part //
     for (int i = 0; i < n_time_points; ++i) {
-        // while (t < time_points[i]) {
-        //     // update birth rates in compartment 0 (main node)
-        //     birth_rate = mu + c_b*(nss-sys_state[0]-(delta*sys_state[1]));
-        //     if (birth_rate < 0) // check for negative rates (possible if e.p.s. > nss)
-        //         birth_rate = 0; 
-        //     react_rates[0] = react_rates[1] = birth_rate;
 
-        //     propensity_sum = 0.0;
-        //     for (int j = 0; j < n_reactions; j++) {
-        //         // calculate global reaction propensity by multiplyin per capita rates with the number of reactants
-        //         react_propen[j] = react_rates[j]*sys_state[state_index[j]];
-        //         // keep track of the sum of global propensities
-        //         propensity_sum += react_propen[j];
-        //     }
+        // while the next time point is reached
+        while (t < time_points[i]) {
+            
+            // update birth rates in compartment 0 (main node)
+            birth_rate = mu + c_b*(nss-sys_state[0]-(delta*sys_state[1]));
+            if (birth_rate < 0) // check for negative rates (possible if e.p.s. > nss)
+                birth_rate = 0; 
+            react_rates[0] = react_rates[1] = birth_rate;
 
-        //     // if there exist any reactions (e.g. all reaction rates > 0, all reactants present at > 0)
-        //     if (propensity_sum != 0.0) {
-        //         // normalize global propensities such that their sum is 1 (forming a pmf)
-        //         for (int j = 0; j < n_reactions; j++) {
-        //             react_probal[j] = react_propen[j]/propensity_sum;
-        //         }
+            propensity_sum = 0.0;
+            for (int j = 0; j < n_reactions; j++) {
+                // calculate global reaction propensity by multiplyin per capita rates with the number of reactants
+                react_propen[j] = react_rates[j]*sys_state[state_index[j]];
+                // keep track of the sum of global propensities
+                propensity_sum += react_propen[j];
+            }
+
+            // if there exist any reactions (e.g. all reaction rates > 0, all reactants present at > 0)
+            if (propensity_sum != 0.0) {
+                // normalize global propensities such that their sum is 1 (forming a pmf)
+                for (int j = 0; j < n_reactions; j++) {
+                    react_probal[j] = react_propen[j]/propensity_sum;
+                }
                 
-        //         // randomly generate a reaction
-        //         reaction_index = random_event_pmf(react_probal);  
+                // randomly generate a reaction
+                reaction_index = random_event_pmf(react_probal);  
             
-        //         // apply the reaction to the state of the system
-        //         for (int j = 0; j < n_pops; j++) {
-        //             sys_state[j] += reactions(reaction_index,j);          
-        //         }
+                // apply the reaction to the state of the system
+                for (int j = 0; j < n_pops; j++) {
+                    sys_state[j] += reactions(reaction_index,j);          
+                }
                 
-        //         // increment time forward
-        //         t += random_sample_exponential(1/propensity_sum);
+                // increment time forward
+                t += random_sample_exponential(1/propensity_sum);
             
-        //     // if no reactions occur with probability > 0 (the system is empty), the state of the system will never change
-        //     } else {
-        //         t += 0.001;
-        //     }
-        // }
+            // if no reactions occur with probability > 0 (the system is empty), the state of the system will never change
+            } else {
+                t += 0.001;
+            }
+        }
+
         // write the current state of the system to the output array
         for (int j = 0; j < n_pops; ++j) {
             sys_state_sample(i,j) = sys_state[j];
