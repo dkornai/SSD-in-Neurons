aab0e84df329b5e7e7e1a77e2c490f1144dfda36
dkornai <danielkornai@gmail.com>
2023-06-26 19:05:33 +0100

    explored new possibilities for the soma graph

--- simulate_gillespie_cpp.cpp
+++ simulate_gillespie_cpp.cpp
@@ -3,14 +3,10 @@
 #include <vector>
 #include <random>
 #include <numeric>
-#include <stdexcept>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/eigen.h>
-
-#include <iostream>
-#include <string> 
 
 namespace py = pybind11;
 
@@ -54,16 +50,16 @@ vec_long get_long_vec_from_np(
 }
 
 
-// draw samples from an exponential with a specified mean 
+// draw samples from an exponential with a specified rate
 double random_sample_exponential(
-    const   double     mean
+    const   double     rate
     ) 
 {
     // initialized only once the first time the function is called
     static std::mt19937 gen(std::random_device{}());
     
     // generate random number from exponential dist.
-    std::exponential_distribution<double> dist(1.0 / mean);
+    std::exponential_distribution<double> dist(rate);
     return dist(gen);
 }
 
@@ -93,7 +89,9 @@ void simulate(
     const   np_mat_long     reactions,                  // 'sys_state' updates corresponding to each possible reaction
     const   np_vec_f64      in_react_rates,             // per capita reaction rates
     const   np_vec_i64      in_state_index,             // indeces of system state variables from which propensity is calculated
-    const   np_vec_f64      in_birthrate_updates_par    // parameters used to update dynamic birth rates
+
+    const   np_vec_f64      in_birthrate_updates_par,   // parameters used to update dynamic birth rates
+    const   long            n_birthrate_updates         // number of birth rate reactions which must be updated
     )
 {
     // c++ versions of numpy arrays
@@ -115,27 +113,36 @@ void simulate(
     const   double          delta           = br_up_par[3];
     
     // variables rewritten throughout simulation
-            double          birth_rate;
-            long            reaction_index;
-
-            vec_double      react_propen(n_reactions);
-            vec_double      react_probal(n_reactions);
-
-            double          propensity_sum  = 0.0;
             double          t               = time_points[0];
 
+            double          birth_rate;                         // birthrate
+            vec_double      prev_state(n_birthrate_updates, -1);// previous state of the nodes with birth rate updates
+            
+            long            reaction_index;
+            vec_double      react_propen(n_reactions);
+            double          propensity_sum  = 0.0;
 
+        
     // actual simulation part //
     for (int i = 0; i < n_time_points; ++i) {
 
         // while the next time point is reached
         while (t < time_points[i]) {
             
-            // update birth rates in compartment 0 (main node)
-            birth_rate = mu + c_b*(nss-sys_state[0]-(delta*sys_state[1]));
-            if (birth_rate < 0) // check for negative rates (possible if e.p.s. > nss)
-                birth_rate = 0; 
-            react_rates[0] = react_rates[1] = birth_rate;
+            // update birth rates in all nodes with active population size control
+            for (int j = 0; j < n_birthrate_updates; j+=2) {
+                // but only if the population sizes changed in the previous iteration
+                if (sys_state[j] != prev_state[j] or sys_state[j+1] != prev_state[j+1]) {
+                    
+                    birth_rate = mu + c_b*(nss-sys_state[j]-(delta*sys_state[j+1]));
+                    if (birth_rate < 0) // check for negative rates (possible if e.p.s. > nss)
+                        birth_rate = 0; 
+                    react_rates[j] = react_rates[j+1] = birth_rate; // set corresponding birth rates for the nodes
+                }
+            }
+            
+            // record the current state of the compartments with active population size control
+            std::copy(sys_state.begin(), sys_state.begin() + n_birthrate_updates, prev_state.begin());
 
             propensity_sum = 0.0;
             for (int j = 0; j < n_reactions; j++) {
@@ -147,13 +154,8 @@ void simulate(
 
             // if there exist any reactions (e.g. all reaction rates > 0, all reactants present at > 0)
             if (propensity_sum != 0.0) {
-                // normalize global propensities such that their sum is 1 (forming a pmf)
-                for (int j = 0; j < n_reactions; j++) {
-                    react_probal[j] = react_propen[j]/propensity_sum;
-                }
-                
-                // randomly generate a reaction
-                reaction_index = random_event_pmf(react_probal);  
+                // get the reaction
+                reaction_index = random_event_pmf(react_propen);  
             
                 // apply the reaction to the state of the system
                 for (int j = 0; j < n_pops; j++) {
@@ -161,11 +163,11 @@ void simulate(
                 }
                 
                 // increment time forward
-                t += random_sample_exponential(1/propensity_sum);
+                t += random_sample_exponential(propensity_sum);
             
             // if no reactions occur with probability > 0 (the system is empty), the state of the system will never change
             } else {
-                t += 0.001;
+                t += 0.1;
             }
         }
 
@@ -177,9 +179,8 @@ void simulate(
 }
 
 
-
+// pybind 
 PYBIND11_MODULE(libgillespie, m)
 {
     m.def("simulate", &simulate, "simulate using gillespie");
 }
-
