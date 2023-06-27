980979ffcb7422e39e7e774b59341b20ef96cbf0
dkornai <danielkornai@gmail.com>
2023-06-27 19:04:27 +0100

    added new graph generation algorithms

--- network_generate.py
+++ network_generate.py
@@ -1,102 +1,143 @@
 import networkx as nx
 from sim_param_from_network import names_from_network
 
-# generate a chain of nodes
-def network_gen_chain(
-        n_nodes     :int, 
-        start_pop   :list[int, int], 
-        bio_param   :dict
-        ):
+def sim_helpers_from_network(
+        network:        nx.DiGraph, 
+        start_pop:      list[int, int],
+        ) ->            tuple[list[str], list[str], list[int]]:
 
-    assert n_nodes > 1, 'chain must have 2+ nodes'
+    # generate names of variables and node
+    vars, node_labels = names_from_network(network)
 
-    # extract the anterograde and retrograde hopping rates
-    hr_ant = float(bio_param['gamma_ant'])
-    hr_ret = float(bio_param['gamma_ret'])
-    assert hr_ant > 0, "anterograde hopping rate must be > 0"
-
-    
-    G = nx.DiGraph()
-    # add main starer node
-    G.add_node('N0', birth_type=2, c_b = float(bio_param['soma_cb']), birth_rate = float(bio_param['soma_br']), nss=float(bio_param['soma_nss']), delta = float(bio_param['delta']), death_rate = float(bio_param['turnover']),)
-
-    # add remaining nodes:
-    for i in range(1, n_nodes):
-        prev_node_name = f"N{i-1}"
-        curr_node_name = f"N{i}"
-
-        G.add_node(curr_node_name, birth_type=0, nss=-1, death_rate = float(bio_param['turnover']),)
-        G.add_edge(prev_node_name, curr_node_name, rate=hr_ant)
-        if hr_ret > 0:
-            G.add_edge(curr_node_name, prev_node_name, rate=hr_ret)
-
-    # extract variable names, and compartment (node) names
-    vars, comp = names_from_network(G)
-
-    # set a given starting state
+    # generate starting state of the system variables
     start_state = []
-    for i in range(n_nodes):
+    for i in range(len(network.nodes())):
         start_state.append(start_pop[0]);start_state.append(start_pop[1])
-
-    return G, vars, comp, start_state
-
-# generate a ring of nodes
-def network_gen_ring(
-        n_nodes     :int, 
-        start_pop   :list[int, int], 
-        bio_param   :dict
-        ):
     
-    assert n_nodes > 2, 'ring must have 3+ nodes'
+
+    return vars, node_labels, start_state
+
+
+# generate the circular network corresponding to the soma
+def net_gen_hub_ring(
+        n_nodes:        int,
+        bio_param:      dict,
+        ) ->            nx.DiGraph:
+    
+    # make a circular graph, and transform it to a directed graph
+    soma_g = nx.cycle_graph(n_nodes)
+    if n_nodes == 1: soma_g.remove_edge(0, 0) # remove self loop if only one node is present
+    soma_g = soma_g.to_directed()
+
+    # set node attributes
+    nx.set_node_attributes(soma_g, 2,                               "birth_type")
+    nx.set_node_attributes(soma_g, float(bio_param['soma_cb']),     "c_b")
+    nx.set_node_attributes(soma_g, float(bio_param['soma_br']),     "birth_rate")
+    nx.set_node_attributes(soma_g, float(bio_param['death_rate']),  "death_rate")
+    nx.set_node_attributes(soma_g, float(bio_param['soma_nss']),    "nss")
+    nx.set_node_attributes(soma_g, float(bio_param['delta']),       "delta")
+    
+    # add diffusion between soma nodes
+    nx.set_edge_attributes(soma_g, float(bio_param['soma_diffusion']), "rate")
+    
+    # relable nodes as to indicate they are part of the soma
+    rename_dict = {i:f'S{i}' for i in range(n_nodes)}
+    nx.relabel_nodes(soma_g, rename_dict, copy=False)
+
+
+    return soma_g
+
+
+
+
+# add a chain of nodes to an existing graph
+def net_gen_line_chain(
+        n_nodes:        int,
+        bio_param:      dict,
+        line_type:     str,
+        attach_graph:   nx.DiGraph,  # graph to which the chain should be attached
+        attach_node:    str,         # name of the node to which the chain should be attached
+        )->             nx.DiGraph:
+
+    assert n_nodes >= 1, 'chain must at least 1 node'
+    assert float(bio_param['gamma_ant']) > 0, "anterograde hopping rate must be > 0"
+
+    # add new nodes and edges    
+    for i in range(n_nodes):
+    
+        prev_node_name = f"{line_type}{i-1}"
+        curr_node_name = f"{line_type}{i}"
+
+        attach_graph.add_node(
+            curr_node_name, 
+            birth_type=0, 
+            nss=-1, 
+            death_rate = float(bio_param['death_rate'])
+            )
+        
+        # add connections to other nodes
+        if i == 0: prev_node_name = attach_node # the precursor of the first node is on the existing graph
+
+        attach_graph.add_edge(prev_node_name, curr_node_name, rate=float(bio_param['gamma_ant']))
+        if float(bio_param['gamma_ret']) > 0:
+            attach_graph.add_edge(curr_node_name, prev_node_name, rate=float(bio_param['gamma_ret']))
+
+
+    return attach_graph
+
+# add a ring of nodes to an existing graph
+def net_gen_line_ring(
+        n_nodes:        int,
+        bio_param:      dict,
+        line_type:      str,
+        attach_graph:   nx.DiGraph,  # graph to which the chain should be attached
+        attach_node:    str,         # name of the node to which the chain should be attached
+        )->             nx.DiGraph:
+    
+    assert n_nodes >= 2, 'ring must have 2 or more nodes'
+    assert float(bio_param['gamma_ant']) > 0, "hopping rate of ring must be > 0"
+
+    # add new nodes and edges    
+    for i in range(n_nodes):
+    
+        prev_node_name = f"{line_type}{i-1}"
+        curr_node_name = f"{line_type}{i}"
+
+        attach_graph.add_node(
+            curr_node_name, 
+            birth_type=0, 
+            nss=-1, 
+            death_rate = float(bio_param['death_rate'])
+            )
+        
+        # add connections to other nodes
+        if i == 0: prev_node_name = attach_node # the precursor of the first node is on the existing graph
+
+        attach_graph.add_edge(prev_node_name, curr_node_name, rate=float(bio_param['gamma_ant']))
+    
+    # attach final node to complete the circle
+    attach_graph.add_edge(curr_node_name, attach_node, rate=float(bio_param['gamma_ant']))
+
+
+    return attach_graph
+
+   
+
+# add a powerlaw tree to an existing graph
+def net_gen_line_powlawtree(
+        n_nodes:        int,
+        bio_param:      dict,
+        line_type:      str,
+        attach_graph:   nx.DiGraph,  # graph to which the chain should be attached
+        attach_node:    str,         # name of the node to which the chain should be attached
+        )->             nx.DiGraph:
+    
+    assert n_nodes >= 1, 'powerlaw tree must at least 1 node'
 
     # extract the anterograde and retrograde hopping rates
-    hr_ant = float(bio_param['gamma_ant'])
-    hr_ret = float(bio_param['gamma_ret'])
-    assert hr_ant > 0, "anterograde hopping rate must be > 0"
-
-    G = nx.DiGraph()
-    # add main starer node
-    G.add_node('N0', birth_type=2, c_b = float(bio_param['soma_cb']), birth_rate = float(bio_param['soma_br']), nss=float(bio_param['soma_nss']), delta = float(bio_param['delta']), death_rate = float(bio_param['turnover']),)
-
-    # add remaining nodes:
-    for i in range(1, n_nodes):
-        prev_node_name = f"N{i-1}"
-        curr_node_name = f"N{i}"
-
-        G.add_node(curr_node_name, birth_type=0, nss=-1, death_rate = float(bio_param['turnover']),)
-        G.add_edge(prev_node_name, curr_node_name, rate=hr_ant)
-        if hr_ret > 0:
-            G.add_edge(curr_node_name, prev_node_name, rate=hr_ret)
-
-    # add edges connecting last node to N0
-    G.add_edge(f"N{n_nodes-1}", 'N0', rate=hr_ant)
-    if hr_ret > 0:
-        G.add_edge('N0', f"N{n_nodes-1}", rate=hr_ret)
-
-
-    # extract variable names, and compartment (node) names
-    vars, comp = names_from_network(G)
-
-    # set a given starting state
-    start_state = []
-    for i in range(n_nodes):
-        start_state.append(start_pop[0]);start_state.append(start_pop[1])
-
-    return G, vars, comp, start_state     
-
-# generate a small tree
-def network_gen_powlaw_tree(
-        n_nodes     :int, 
-        start_pop   :list[int, int], 
-        bio_param   :dict
-        ):
-    
-    assert n_nodes > 1, 'powerlaw tree must have 2+ nodes'
-
-    # extract the anterograde and retrograde hopping rates
-    hr_ant = float(bio_param['gamma_ant'])
-    hr_ret = float(bio_param['gamma_ret'])
-    assert hr_ant > 0, "anterograde hopping rate must be > 0"
+    # gamma_ant = float(bio_param['gamma_ant'])
+    # gamma_ret = float(bio_param['gamma_ret'])
+    assert float(bio_param['gamma_ant']) > 0, "anterograde hopping rate must be > 0"
 
     # generate starting point of network
     G = nx.random_powerlaw_tree(n_nodes, gamma = 5, tries =100000, seed = 123)
@@ -108,36 +149,31 @@ def network_gen_powlaw_tree(
 
     # set attributes for all nodes
     nx.set_node_attributes(H, 0, "birth_type")
-    nx.set_node_attributes(H, float(bio_param['turnover']), "death_rate")
+    nx.set_node_attributes(H, float(bio_param['death_rate']), "death_rate")
     nx.set_node_attributes(H, -1, "nss")
-
-    # change attributes for node 0
-    nx.set_node_attributes(H, {0:2}, "birth_type")
-    nx.set_node_attributes(H, {0:float(bio_param['soma_cb'])}, "c_b")
-    nx.set_node_attributes(H, {0:float(bio_param['soma_br'])}, "birth_rate")
-    nx.set_node_attributes(H, {0:float(bio_param['soma_nss'])}, "nss")
-    nx.set_node_attributes(H, {0:float(bio_param['delta'])}, "delta")
     
-    # set hopping rates
-    nx.set_edge_attributes(H, {e:hr_ant for e in out_edge}, "rate")
-    if hr_ret == 0:
+
+    # set hopping rates for anterograde edges
+    nx.set_edge_attributes(H, {e:float(bio_param['gamma_ant']) for e in out_edge}, "rate")
+    
+    # set hopping rates for retrograde edges (or delete if retrograde rate is 0)
+    if float(bio_param['gamma_ret']) == 0:
         for e in in_edge:
             H.remove_edge(e[0], e[1])
-
     else:
-        nx.set_edge_attributes(H, {e:hr_ret for e in in_edge}, "rate")
+        nx.set_edge_attributes(H, {e:float(bio_param['gamma_ret']) for e in in_edge}, "rate")
     
+
     # rename nodes for compatibility with other functions
-    rename_dict = {i:f'N{i}' for i in range(n_nodes)}
+    rename_dict = {i:f'{line_type}{i}' for i in range(n_nodes)}
     nx.relabel_nodes(H, rename_dict, copy=False)
     
+    # combine the graphs, and add the connecting edges
+    out_graph = nx.DiGraph()
+    out_graph.add_edges_from(list(attach_graph.edges(data=True))+list(H.edges(data=True)))
+    out_graph.add_nodes_from(list(attach_graph.nodes(data=True))+list(H.nodes(data=True)))
+    out_graph.add_edge(list(H.nodes())[0], attach_node, rate=float(bio_param['gamma_ant']))
+    if float(bio_param['gamma_ret']) > 0: out_graph.add_edge(attach_node, list(H.nodes())[0], rate=float(bio_param['gamma_ret']))
 
-    # extract variable names, and compartment (node) names
-    vars, comp = names_from_network(H)
 
-    # set a given starting state
-    start_state = []
-    for i in range(n_nodes):
-        start_state.append(start_pop[0]);start_state.append(start_pop[1])
-
-    return H, vars, comp, start_state
+    return out_graph
