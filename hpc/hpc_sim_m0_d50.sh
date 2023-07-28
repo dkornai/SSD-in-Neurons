84f75d107ea16e8303559df3b2ce0387ba38efce
dkornai <danielkornai@gmail.com>
2023-07-28 21:40:55 +0200

    changed execution time

--- hpc/hpc_sim_m0_d50.sh
+++ hpc/hpc_sim_m0_d50.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PBS -N ssd_sim_long
-#PBS -l walltime=36:00:00
+#PBS -l walltime=72:00:00
 #PBS -l select=1:ncpus=256:mem=512gb
 
 cd /rds/general/user/dkornai/home/Projects/SSD-in-Neurons
